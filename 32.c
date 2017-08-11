a59d7ffb1aa0fd77e90b1b714610f22b403dcb12
dhivya97 <30342650+dhivya97@users.noreply.github.com>
2017-08-11 16:46:33 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void main()
+{
+    char s[200];
+    int count = 0, j;
+ 
+    printf("enter the string\n");
+    scanf("%[^\n]s", s);
+    for (j = 0;s[j] != '\0';j++)
+    {
+        if (s[j] == ' ')
+            count++;    
+    }
+    printf("number of words in given string are: %d\n", count + 1);
+}
